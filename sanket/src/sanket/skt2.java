8df217553669270a25675e89df8f16e7b4813f9d
sdac <sdac@sdac-KY650AA-ACJ-CQ3030IL>
2024-01-16 09:20:32 +0530

    v.1

---
+++ sanket/src/sanket/skt2.java
@@ -0,0 +1,13 @@
+package sanket;
+
+public class skt2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+
+System.out.println("hello v.1");
+
+	}
+
+}
